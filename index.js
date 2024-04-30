15de8871b301b3fa6f19e2e717e1e5e1c87c0fb7
AydenGrace <pa.ce.hepl@gmail.com>
2024-04-30 09:24:00 +0200

    Front link

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const config = require("./database/config");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+const allowedOrigin = "https://todolist-front-zeta.vercel.app/";
 
 app.use(express.json());
-app.use(
-  cors({
-    origon: "*",
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigin);
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 const routes = require("./routes");
 app.use(routes);
@@ -22,7 +25,7 @@ mongoose
   })
   .catch((e) => console.error(e));
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server started on port ${PORT}`);
   console.log("\x1b[36m%s\x1b[0m", `http://localhost:${PORT}/api/todos`);
 });
